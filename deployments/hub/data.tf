6cb289e03d88057706e7b676fcd35ae1d5762ec9
Veera Bhadra Mangunuru <veerabhadra.devops@gmail.com>
2026-01-10 00:09:18 -0600

    test

--- deployments/hub/data.tf
+++ deployments/hub/data.tf
@@ -1,1 +1,9 @@
 data "azurerm_client_config" "current" {}
+
+data "azuread_user" "admin_user" {
+  user_principal_name = "questopshub.microsoft_gmail.com#EXT#@questopshubmicrosoftgmail.onmicrosoft.com"
+}
+
+data "azuread_service_principal" "hub_service_principal" {
+  display_name = "hub_service_principal"
+}
